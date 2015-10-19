1c6688bcf3a78e48b8bbd4787aca5835a46320df
gszsilva <g.szsilva@gmail.com>
2015-10-19 11:47:56 -0200

    V0.1.2 - Atualização e novos Beans (Hosped!, Pag, Formapg, Reserva, Endereco)

--- src/br/com/fiap/beans/Hospedagem.java
+++ src/br/com/fiap/beans/Hospedagem.java
@@ -2,4 +2,42 @@ package br.com.fiap.beans;
 
 public class Hospedagem {
 
+	private String dtCheckin;
+	private Reserva reserva;
+	private Funcionario funcionario;
+	
+	public Hospedagem(String dtCheckin, Reserva reserva, Funcionario funcionario) {
+		super();
+		this.dtCheckin = dtCheckin;
+		this.reserva = reserva;
+		this.funcionario = funcionario;
+	}
+
+	public Hospedagem() {
+		super();
+	}
+
+	public String getDtCheckin() {
+		return dtCheckin;
+	}
+
+	public void setDtCheckin(String dtCheckin) {
+		this.dtCheckin = dtCheckin;
+	}
+
+	public Reserva getReserva() {
+		return reserva;
+	}
+
+	public void setReserva(Reserva reserva) {
+		this.reserva = reserva;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 }
